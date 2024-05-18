66b83b6ac4b9c9bc432b3d16e74897f3bffe14f0
SimonBarilaro <162722001+SimonBarilaro@users.noreply.github.com>
2024-05-18 15:42:11 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,26 @@
 def enumerate_list(list):
-    return list
+    indice=0
+    nuevo_indice = 0
+    milista=[]
+    while indice<len(list):
+        elemento = list[indice]
+        if elemento:
+            palabra=f"{nuevo_indice}. {elemento}"
+            milista.append(palabra)
+            nuevo_indice += 1
+        indice+=1
+    return milista
 
 
 def enumerate_backwards(list):
-    return list
+    indice=0
+    nuevo_indice = 0
+    milista=[]
+    while indice<len(list):
+        elemento = list[indice]
+        if elemento:
+            palabra=f"{nuevo_indice}. {elemento[::-1]}"
+            milista.append(palabra)
+            nuevo_indice += 1
+        indice+=1
+    return milista
